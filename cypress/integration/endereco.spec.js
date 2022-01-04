2a824817454fbe5e14988641aa91ce7432081d72
name <anderson.netster2012@gmail.com>
2022-01-03 21:42:37 -0300

    teste de page object

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import EnderecoPage from '../support/page-objects/endereco.page'
+
 describe('funcinalidade endereços faturamento e entrega', () => {
 
     beforeEach(() => {
@@ -11,8 +13,8 @@ describe('funcinalidade endereços faturamento e entrega', () => {
     });
 
 
-    it('deve fazer cadastrpo de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+    it.only('deve fazer cadastrpo de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('dayane', 'santana', 'daytransportes', 'Catar','fausto ribeiro', '3', 'Sol Nascente', 'Pernambuco', '05280100', '11965650796', 'dayane@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso') 
     });
 });
